ccc9d1633047c5dc4012f1b1303b270518ff17fc
Mithilesh Tiwari <Mithilesh@crowdbotics.com>
2022-07-22 13:46:16 +0530

    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -8,7 +8,16 @@ const Untitled1 = () => {
     position: 'relative',
     height: '101%'
   }}>
-      </View>;
+      <View style={{
+      left: 26,
+      top: 52,
+      position: "absolute",
+      height: 80,
+      width: 150,
+      backgroundColor: "#E4E4E4",
+      borderRadius: 0,
+      color: "#777777"
+    }}></View></View>;
 };
 
 export default Untitled1;
