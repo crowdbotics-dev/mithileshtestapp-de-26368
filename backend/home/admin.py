802d5c8688795ee39503a3fd4c13ccffb9ec1e55
Crowdbotics <engineering@crowdbotics.com>
2022-08-25 11:38:34 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import YRR
+
+admin.site.register(YRR)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,7 @@
+from django.conf import settings
 from django.db import models
+
+
+class YRR(models.Model):
+    "Generated Model"
+    kytfyty = models.BigIntegerField()
