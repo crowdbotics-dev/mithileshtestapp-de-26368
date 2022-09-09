741844d4ae17d41dce79bf453b747f43bf72a420
Mithilesh Tiwari <Mithilesh@crowdbotics.com>
2022-09-09 15:19:28 +0530

    Crowdbotics Studio: Update app screens

---
+++ screens/Untitled2/index.js
@@ -0,0 +1,39 @@
+import { TextInput } from "react-native";
+import { Text } from "react-native";
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Untitled2 = () => {
+  return <View style={_styles.xhIffAgG}>
+      <Text style={_styles.ooTGmhES}>Lorem ipsum…</Text><TextInput style={_styles.IwIZnzNT}></TextInput></View>;
+};
+
+export default Untitled2;
+
+const _styles = StyleSheet.create({
+  xhIffAgG: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  },
+  ooTGmhES: {
+    left: 148.49242404917504,
+    top: 63.6396103067893,
+    position: "absolute",
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
+  },
+  IwIZnzNT: {
+    left: 148.49242404917504,
+    top: 235.11300474452716,
+    position: "absolute",
+    backgroundColor: "#ffffff",
+    borderColor: "#cccccc",
+    width: 150,
+    height: 30
+  }
+});
